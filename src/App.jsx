ec907711a3cef723976c11f04c27c02520fd6110
Mihir Bhadak <98308594+mihirbhadak@users.noreply.github.com>
2024-08-03 08:12:46 +0000

    modified:   package.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mihir Bhadak",
+  title: "Full-Stack Developer",
+  email: "mihirbhadak@gmail.com",
+  gitHub: "mihirbhadak",
+  instagram: "mihir_bhadak",
+  linkedIn: "mihirbhadak",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mihir_bhadak",
+  youTube: "Mihir Bhadak",
 };
 
 const primaryColor = "#4E567E";
